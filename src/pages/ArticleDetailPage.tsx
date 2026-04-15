2422f41261923e32eb576bd6d6930e3f1824585e
gabriochee <gabypla2005@gmail.com>
2026-04-15 22:59:46 +0200

    Début de la page détaillée. Requête avec Tanstack en place.

--- src/pages/ArticleDetailPage.tsx
+++ src/pages/ArticleDetailPage.tsx
@@ -1,3 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
 export default function ArticleDetailPage() {
-  return <h1>Détail article</h1>;
+  let params = useParams();
+
+  if (!params.id) return;
+
+  let articleId: string = params.id;
+
+  const {isPending, isError, data, error} = useQuery({
+    queryKey: ['article'],
+    queryFn: () => fetch(`/api/articles/${articleId}`).then((res) => res.json()),
+  });
+
+  if (isPending){
+    return <p>Chargement...</p>;
+  }
+
+  if (isError){
+    console.log(`Une erreur est survenue : ${error.message}`);
+    return <p>Une erreur est survenue.</p>;
+  }
+
+  return <>
+    <p>{data.title}</p>
+    <p>{data.description}</p>
+    <p>{data.price}</p>
+    <p>{data.category}</p>
+    <p>{data.size}</p>
+    <p>{data.condition}</p>
+    <p>{data.userName}</p>
+    <p>{data.createdAt}</p>
+  </>
 }
