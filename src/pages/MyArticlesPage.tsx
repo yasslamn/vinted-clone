e5bae5be60989ec74765e86f084188d044a7df58
yasslamn <yasmin.slamani@gmail.com>
2026-04-16 21:51:55 +0200

    gomenasai edson j'avais pas capté à quoi il servait ton css mais en vrai oui t'as raison

--- src/pages/MyArticlesPage.tsx
+++ src/pages/MyArticlesPage.tsx
@@ -60,7 +60,7 @@ export default function MyArticlesPage() {
   }
   // Else return the list of articles with the ArticleCard component
   return (
-    <div className="flex flex-wrap gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {getUserArticlesQuery.data.map((article) => (
         <ArticleCard
           key={article.id}
