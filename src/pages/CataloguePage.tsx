08f563dad33d96df03eefb9f0e6b5b4e53f77f52
“EdsonXIV” <edson.darcel@hotmail.co.uk>
2026-04-16 20:35:47 +0200

    added on edit in ArticleCard in the catalogPage

--- src/pages/CataloguePage.tsx
+++ src/pages/CataloguePage.tsx
@@ -57,7 +57,7 @@ export default function CataloguePage() {
       />
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
         {data.map((article) => (
-          <ArticleCard key={article.id} article={article} />
+          <ArticleCard key={article.id} article={article} onEdit={() => {}} />
         ))}
       </div>
     </div>
